bb749538a534157c39c7d11f2cbe03c2e9d806f1
abderrahmaneMustapha <abderrahmanemustapha030898@gmail.com>
2019-11-18 18:33:41 +0100

    add time delta

---
+++ python/Time Delta.py
@@ -0,0 +1,33 @@
+#!/bin/python3
+
+import math
+import os
+import random
+import re
+import sys
+from datetime import datetime as dt
+
+# Complete the time_delta function below.
+def time_delta(t1, t2):
+    t1 = dt.strptime(t1,'%a %d %b %Y %H:%M:%S %z');
+    t2 = dt.strptime(t2,'%a %d %b %Y %H:%M:%S %z');
+
+    return str(int(abs((t1 - t2).total_seconds())))
+        
+        
+
+if __name__ == '__main__':
+    fptr = open(os.environ['OUTPUT_PATH'], 'w')
+
+    t = int(input())
+
+    for t_itr in range(t):
+        t1 = input()
+
+        t2 = input()
+
+        delta = time_delta(t1, t2)
+
+        fptr.write(delta + '\n')
+
+    fptr.close()
